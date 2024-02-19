1e2894ec0bcebc7a458f9d47e5c77d3b0888187a
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-02-19 15:31:08 +0300

    101

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,4 +1,4 @@
-#include "sort.h"
+include "sort.h"
 
 /**
  * list_len - function returns length of list
@@ -42,4 +42,48 @@ void switch_nodes(listint_t **list, listint_t **p)
                 *list = three;
         two->prev = three;
         *p = three;
+}
+/**
+ *  cocktail_sort_list - function sorts a doubly linked list using
+ * the cocktail sort algorithm
+ * @list: pointer to list
+ */
+void cocktail_sort_list(listint_t **list)
+{
+        listint_t *p;
+        int sorted = 0;
+
+        if (!list || !*list || list_len(*list) < 2)
+                return;
+        p = *list;
+        while (!sorted)
+        {
+                sorted = 1;
+                while (p->next)
+                {
+                        if (p->n > p->next->n)
+                        {
+                                sorted = 0;
+                                switch_nodes(list, &p);
+                                print_list(*list);
+                        }
+                        else
+                                p = p->next;
+                }
+                if (sorted)
+                        break;
+                p = p->prev;
+                while (p->prev)
+                {
+                        if (p->n < p->prev->n)
+                        {
+                                sorted = 0;
+                                p = p->prev;
+                                switch_nodes(list, &p);
+                                print_list(*list);
+                        }
+                        else
+                                p = p->prev;
+                }
+        }
 }
